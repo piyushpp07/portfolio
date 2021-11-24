ef7b5d76c2671cec8e7a12a96e5f3c4e43fc6fdd
piyushpp07 <piyushpar7@gmail.com>
2021-11-24 19:19:02 +0530

    Ad

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react'
 import { Navbar, Container, Nav, NavLink } from 'react-bootstrap'
 import { motion } from 'framer-motion';
 import { Link } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
 import DarkModeToggle from "react-dark-mode-toggle";
+import Skills from '../skills/Skills';
 export default function Header(props) {
    const { isDark } = props;
    const [isDarkMode, setIsDarkMode] = useState(isDark);
@@ -15,7 +17,7 @@ export default function Header(props) {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end" activeKey="/home">
                <Nav >
-                  <NavLink style={{ color: isDark ? "white" : "black", fontWeight: 'bold' }}>Skills</NavLink>
+                  <NavLink style={{ color: isDark ? "white" : "black", fontWeight: 'bold' }}><ScrollLink to={Skills}>Skills</ScrollLink></NavLink>
                   <NavLink style={{ color: isDark ? "white" : "black", fontWeight: 'bold' }}>Experience</NavLink>
                   <NavLink style={{ color: isDark ? "white" : "black", fontWeight: 'bold' }}>Contact Me</NavLink>
                </Nav>
